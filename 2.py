afc7b892c6ce7d10a8fb19bee06f2d2ee8a8bc5b
kd699 <kd69931@gmail.com>
2026-03-14 10:38:03 +0200

    sdf

---
+++ 2.py
@@ -0,0 +1,17 @@
+# ? получение данных
+# name = input("Введите имя:")
+# print("Вас зовут", name)
+# print(f"Вас зовут {name}")
+
+# ? int - переводит в число
+# num1 = input()
+# num1 = int(num1)
+# num2 = int(input())
+
+# print(num1 + num2)
+
+# ? str - переводит в строки
+num1 = str(5)
+num2 = str(10)
+
+print(num1 + num2)
